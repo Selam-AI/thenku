bfb8a67c165a36961913752cb1436c6e5a1f3cdc
Tahmid <tahmid.ae@gmail.com>
2025-01-03 18:11:23 +0600

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Chatbot from './features/homepage/middle';
 import Glance from './features/homepage/glance';
+import Homenav from './features/homepage/Homenav';
 
 function App() {
   return (
     <>
-<Glance/>
+
+<Homenav/>
 
 
 <Chatbot/>
