6b31213835259964f9a6bd527f2ef2a432ce283b
Tahmid <tahmid.ae@gmail.com>
2025-01-07 07:29:16 +0600

    Commit 7-7.5

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-    const JWT = require ('jsonwebtoken');
+const JWT = require('jsonwebtoken');
+const cookie = require('cookie');
 
 //models
 const userSchema = new mongoose.Schema({
@@ -11,9 +12,9 @@ const userSchema = new mongoose.Schema({
      email: {
     type: String,
     required: [true, 'Email is Required'],
-    unique:true,s
+    unique:true,
     },
-    passsword: {
+    password: {
     type: String,
     required: [true, 'Passsword is required'],
     minlength:[6,'Passsword length should be 6 character long'],
@@ -35,7 +36,7 @@ userSchema.pre('save', async function (next) {
         next();
     }
     const salt = await bcrypt.genSalt(10);
-    this.passsword = await bcrypt.hash(this.passsword, salt);
+    this.passsword = await bcrypt.hash(this.password, salt);
     next();
 });
 
@@ -48,8 +49,8 @@ userSchema.methods.matchPassword = async function(password) {
 //Sign Token
 userSchema.methods.getSignedToken = function (res) {
     const accessToken = JWT.sign({ id: this.id }, process.env.JWT_ACCESS_SECRET, { expiresIn: JWT_ACCESS_EXPIREIN });
-    const refreshToken = JWT.sign({ id: this.id }, process.env.JWT_REFRESH_TOKEN, { expiresIn: process.envJWT_REFRESH_EXPIREIN });
-    res.cookie('refreshToken', `${refreshToken}`, { maxAge: 86400 * 7000, httpOnly: true });
+    const refreshToken = JWT.sign({ id: this.id }, process.env.JWT_REFRESH_TOKEN, { expiresIn: process.env.JWT_REFRESH_EXPIREIN });
+    res.cookie('refreshToken', `${refreshToken}`, { maxAge: 86400 * 7000, httpOnly: true, });
 };
 
 
